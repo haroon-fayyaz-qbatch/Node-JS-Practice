7c7b752864fa2456d05f7ac0ce1a418d6a7fff4d
haroon-fayyaz-qbatch <haroon.fayyaz@qbatch.com>
2021-07-29 12:37:32 +0500

    Folder Deleted fs

--- fileSystemModule.js
+++ fileSystemModule.js
@@ -2,8 +2,17 @@ const fs = require('fs');
 fs.mkdir('tutorial',err => {
 if(err){
   console.log(err);
+  fs.rmdir('tutorial',err => {
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log("Folder Successfully Deleted");
+    }
+  })
 }
 else{
+
   console.log("folder successfully created");
 }
 });
