7d6292cbb3d0400798caab31270a8e5c4944ac4c
haroon-fayyaz-qbatch <haroon.fayyaz@qbatch.com>
2021-07-30 11:06:32 +0500

    Classes Created

---
+++ Car.js
@@ -0,0 +1,8 @@
+require("./Vehicle");
+
+class Car extends Vehicle {
+  constructor() {
+    super();
+    this.noOfDoors;
+  }
+}
---
+++ Vehicle.js
@@ -0,0 +1,15 @@
+class Vehicle {
+  constructor() {
+    this.name;
+    this.model;
+  }
+  getName() {
+    return this.name;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+}
+
+module.exports = Vehicle;
