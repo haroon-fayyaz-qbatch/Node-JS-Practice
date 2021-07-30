ad85b21c1c8fdd2dd591e2a75e0f2ac7ee09985e
haroon-fayyaz-qbatch <haroon.fayyaz@qbatch.com>
2021-07-30 11:48:45 +0500

    Promise All

--- Promises/promise.js
+++ Promises/promise.js
@@ -1,14 +1,20 @@
-const promise = new Promise((resolve, reject) => {
+const promise1 = new Promise((resolve, reject) => {
   setTimeout(() => {
     // reject(Error('Promise Rejected'));
-resolve('Resolved');
+resolve(1);
+  },2000);
+});
+
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // reject(Error('Promise Rejected'));
+resolve(9);
   },1000);
 });
 
-promise.then((res) => {
+
+Promise.all([promise1, promise2]).then((res) => {
   console.log(res);
 });
 
-promise.catch((err) => {
-  alert(err);
-})
